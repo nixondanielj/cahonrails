bb5bb1293161f6b94fe78cb2498e731432cdbd02
Dan Nixon <nixon.daniel.j@gmail.com>
2014-01-25 19:28:05 -0500

    added login logic

--- app/assets/javascripts/client/controllers/HomeCtrl.js
+++ app/assets/javascripts/client/controllers/HomeCtrl.js
@@ -1,8 +1,7 @@
 app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
     $scope.loginFM = new LoginFM();
     $scope.login = function(){
-        alert($scope.token);
-        $http.post('login', $scope.loginFM, $scope.token).success(function(){
+        $http.post('login', $scope.loginFM).success(function(){
             alert('success');
         });
     };
--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,19 +1,36 @@
 class AuthenticationController < ApplicationController
     
     def login
-        logger.debug params[:new]
-        if params[:new]
-            logger.debug 'creating...'
+        logger.debug params[:isNew]
+        @user = User.find :email => params[:email]
+        if !@user.nil?
+            if @user.password == params[:password]
+                clear_old_tokens
+                @token = Token.new
+                @token.value = 'faketoken'
+            else
+                head :forbidden
+            end
+        elsif params[:isNew]
             @user = User.new
-            @user.email = params[:email]
             @user.password = params[:password]
-            logger.debug @user
+            @user.email = params[:email]
+            @user.save
         else
-            logger.debug 'logging in...'
+            head :forbidden
         end
+        render :json => @token
     end
     
     def logout
         
+    end
+    
+    private
+    def clear_old_tokens
+        Token.find(:active => true).each do |t|
+            t.active = false
+            t.save
+        end
     end
 end
