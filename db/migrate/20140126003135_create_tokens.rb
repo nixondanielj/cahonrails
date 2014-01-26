5e1e1abd927ffdffc6cde043e8de57b8b7f75806
Dan Nixon <nixon.daniel.j@gmail.com>
2014-01-25 19:36:51 -0500

    added user token relationship

--- db/migrate/20140126003135_create_tokens.rb
+++ db/migrate/20140126003135_create_tokens.rb
@@ -2,7 +2,7 @@ class CreateTokens < ActiveRecord::Migration
   def change
     create_table :tokens do |t|
       t.string :value
-      t.int :user_id
+      t.belongs_to :user
 
       t.timestamps
     end
