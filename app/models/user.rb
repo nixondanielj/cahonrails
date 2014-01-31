1b48d02d9a533efa994827a6982871e080b27798
Dan Nixon <nixon.daniel.j@gmail.com>
2014-01-30 22:29:26 -0500

    added validators to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
     has_many :token
+    validates :email, presence: true
+    validates :password, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
