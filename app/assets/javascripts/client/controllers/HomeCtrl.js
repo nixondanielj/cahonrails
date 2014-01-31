c3b5cc30b944500b83744b5229be75775bbc4ed6
Dan Nixon <nixon.daniel.j@gmail.com>
2014-01-30 22:06:09 -0500

    added debugging code

--- app/assets/javascripts/client/controllers/HomeCtrl.js
+++ app/assets/javascripts/client/controllers/HomeCtrl.js
@@ -1,6 +1,7 @@
 app.controller('HomeCtrl', ['$scope', 'AuthService', function($scope, authService){
     $scope.loginFM = new LoginFM();
     $scope.login = function(){
+        alert(JSON.stringify($scope.loginFM));
         authService.login($scope.loginFM).success(function(){
             alert('success');
         }).error(function(data, status){
